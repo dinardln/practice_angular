76934e61f9024fc67019d32e0388509c183076ea
Dina Rudelson <dinardln@umich.edu>
2016-04-20 15:39:24 -0400

    adding in order functionality

--- www/js/controllers/mainController.js
+++ www/js/controllers/mainController.js
@@ -1,4 +1,4 @@
-app.controller('mainController', ['$scope', '$http' ,'openticket', function($scope, $http, openticket) {
+app.controller('mainController', ['$scope', '$http' ,'$window', function($scope, $http, $window) {
 	$scope.tickets = [];
       $scope.update = function(ticket) {
         $http({
@@ -12,15 +12,52 @@ app.controller('mainController', ['$scope', '$http' ,'openticket', function($sco
         }).error(function(err) { 
               return err; 
         }); 
-        console.log($scope.tickets);
       };
+}]);
+
+app.controller('orderController', ['$scope', '$http','$window', function($scope, $http, $window) {
+    $scope.items = [];
+    apiKey = "0b94a8f976834586b839c521d22666b3";
+    $scope.showorders = function(id) {
+    	$scope.id = id;
+      	//console.log(id);
+    	$http({
+			cache: false,
+			url: 'https://api.omnivore.io/0.1/locations/yiKpK5Bi/tickets/'+id+'/items',
+			method: "GET",
+			headers: {'Api-Key' : apiKey },
+		}).success(function(data) { 
+			//console.log(data);
+          	$scope.items.push(data);
+          	$window.location.href = "/js/views/order.html";	 
+        }).error(function(err) { 
+              return err; 
+        });  
+    }
+
+    $scope.placeorder = function(order_id) {
+    	$http.get('https://api.omnivore.io/0.1/locations/yiKpK5Bi/menu/items/'+$order_id , {headers:{'Api-Key' : apiKey }}).success(function(data) { 
+			  console.log(data);
+              $scope.item_info = data; 
+            }) 
+            .error(function(err) { 
+              return err; 
+            }); 
+  //   	$http({
+		// 	cache: false,
+		// 	url: 'https://api.omnivore.io/0.1/locations/yiKpK5Bi/tickets/'+$scope.id+'/items',
+		// 	method: "POST",
+		// 	headers: {'Api-Key' : apiKey },
+		// 	data: {"menu_item": ticket.employee_id, "quantity": ticket.order_type, "price_level": ticket.revenue_center, "comment": , "modifiers": []}
+		// }).success(function(data) { 
+  //         $scope.tickets.push(data); 
+  //       }).error(function(err) { 
+  //             return err; 
+  //       }); 
+    }
 
 
-
-      $scope.place_order = function(id) {
-      	console.log(id);
-
-      }
+     
 }]);
 
 
